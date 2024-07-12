9c0cf3dda953a15539bc37effc7e2d9993630e7e
Archfx <cybx@live.com>
2024-07-12 13:48:41 -0400

    build script updated

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+set -e
+
+BL=$PWD/treble_aosp
+BD=$PWD/duo-de/builds
+BV=$1
+
+
+echo "--> Setting up build environment"
+source build/envsetup.sh &>/dev/null
+mkdir -p $BD
+lunch treble_arm64_bgN-ap2a-userdebug
 make PostureProcessor
-adb install --staged out/target/product/tdgsi_arm64_ab/system/system_ext/priv-app/PostureProcessor/PostureProcessor.apk
-adb reboot
+# adb install --staged out/target/product/tdgsi_arm64_ab/system/system_ext/priv-app/PostureProcessor/PostureProcessor.apk
+# adb reboot
